bd8726d11727f51976d72b606cdf1674f57c6529
steadiestt <dlrnak158@naver.com>
2022-08-27 15:20:07 +0900

    ProductInfo 판매자 표기 미완

--- client/src/components/views/DetailProductPage/Sectoins/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sectoins/ProductInfo.js
@@ -3,14 +3,10 @@ import { Descriptions } from 'antd';
 
 function ProductInfo(props) {
 
-  // const clickHandler = () => {
-
-  // }
-
   return (
     <div>
       <Descriptions title="Info" bordered>
-      {/* <Descriptions.Item label="판매자">{props.detail.writer}</Descriptions.Item> */}
+      {/* <Descriptions.Item label="판매자">{props.detail.writer.name}</Descriptions.Item> */}
       <Descriptions.Item label="Price">{props.detail.price}</Descriptions.Item>
       <Descriptions.Item label="Description">{props.detail.description}</Descriptions.Item>
       </Descriptions>
@@ -19,11 +15,6 @@ function ProductInfo(props) {
       <br />
       <br />
       
-      {/* <div style={{ display: 'flex', justifyContent: 'center'}}>
-        <Button size='large' shape='round' onClick={clickHandler}>
-            Edit
-        </Button>
-      </div> */}
     </div>
   )
 }
